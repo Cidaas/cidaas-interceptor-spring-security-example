e197cd293837e7973e9497943363418a6dfbcb27
Dinesh Babu <dineshbabu.s@widas.in>
2019-04-30 18:02:29 +0530

    First commit

---
+++ src/main/java/de/cidaas/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package de.cidaas.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import de.cidaas.interceptor.config.JwtWebSecurityConfigurer;
+
+@Configuration
+@PropertySource("classpath:cidaas_config.properties")
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private Environment env;
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();		
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("Authorization");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
+		JwtWebSecurityConfigurer.forRS256(env.getProperty("client_id"), env.getProperty("base_url"))
+				.configure(http).authorizeRequests()				
+				.antMatchers(HttpMethod.GET, "/myprofile").authenticated()
+				.antMatchers(HttpMethod.GET, "/v1/**").authenticated()
+				.antMatchers(HttpMethod.GET, "/employeelist").hasRole("HR")
+				.antMatchers(HttpMethod.GET, "/holidaylist").hasAuthority("holidaylist:read")
+				.antMatchers(HttpMethod.GET, "/holidayandemployeelist").hasAuthority("holidaylist:read")
+				.antMatchers(HttpMethod.GET, "/holidayandemployeelist").hasRole("HR")
+				.antMatchers(HttpMethod.GET, "/localholidaylist").permitAll()
+				.antMatchers(HttpMethod.GET, "/leavetype").denyAll();
+		
+		
+	}
+}
---
+++ src/main/java/de/cidaas/controller/APIController.java
@@ -0,0 +1,127 @@
+package de.cidaas.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletRequest;
+
+@Controller
+@Component
+@ComponentScan(basePackages = "de.cidaas.controller")
+public class APIController {
+
+    @RequestMapping("/myprofile")
+    @ResponseBody
+	public Map<String,String> myprofile() {    	
+		Map<String,String> profileObj=new HashMap<String,String>();
+		profileObj.put("firstName", "john");
+		profileObj.put("lastName", "wiliams");
+		profileObj.put("role", "USER");
+		return profileObj;
+	}
+    
+    @RequestMapping("/v1/api/myprofile1")
+    @ResponseBody
+	public Map<String,String> myprofile1() {    	
+		Map<String,String> profileObj=new HashMap<String,String>();
+		profileObj.put("firstName", "wiliams");
+		profileObj.put("lastName", "john");
+		profileObj.put("role", "USER");
+		return profileObj;
+	}
+    @RequestMapping("/v1/api/myprofile2")
+    @ResponseBody
+	public Map<String,String> myprofile2() {    	
+		Map<String,String> profileObj=new HashMap<String,String>();
+		profileObj.put("firstName", "wiliams");
+		profileObj.put("lastName", "john");
+		profileObj.put("role", "USER");
+		return profileObj;
+	}
+    
+    
+    
+    @RequestMapping("/employeelist")
+    @ResponseBody
+	public List<Object> employeeList(ServletRequest request) {		
+		List<Object> empList=new ArrayList<Object>();
+		Map<String,String> profileObj1=new HashMap<String,String>();
+		profileObj1.put("firstName", "john");
+		profileObj1.put("lastName", "wiliams");
+		profileObj1.put("role", "USER");
+		empList.add(profileObj1);
+		Map<String,String> profileObj2=new HashMap<String,String>();
+		profileObj2.put("firstName", "David");
+		profileObj2.put("lastName", "Johnson");
+		profileObj2.put("role", "USER");
+		empList.add(profileObj2);
+		return empList;
+	}
+	
+	//  checks if a caller has one of the scopes defined in scopes. 
+	
+	@RequestMapping("/holidaylist")	
+	@ResponseBody
+	public List<Object> holidayList(ServletRequest request) {		
+		List<Object> holidayList=new ArrayList<Object>();
+		Map<String,String> holidayObj1=new HashMap<String,String>();
+		holidayObj1.put("Date", "1-1-2019");
+		holidayObj1.put("reason", "New year");
+		holidayList.add(holidayObj1);
+		Map<String,String> holidayObj2=new HashMap<String,String>();
+		holidayObj2.put("Date", "25-12-2019");
+		holidayObj2.put("reason", "Christmas Day");
+		holidayList.add(holidayObj2);
+		return holidayList;
+	}	
+	
+	@RequestMapping("/localholidaylist")
+	@ResponseBody
+	public List<Object> localHolidayList(ServletRequest request) {
+		List<Object> holidayList=new ArrayList<Object>();
+		Map<String,String> holidayObj1=new HashMap<String,String>();
+		holidayObj1.put("Date", "1-1-2019");
+		holidayObj1.put("reason", "New year");
+		holidayList.add(holidayObj1);
+		Map<String,String> holidayObj2=new HashMap<String,String>();
+		holidayObj2.put("Date", "25-3-2019");
+		holidayObj2.put("reason", "Christmas Day");
+		holidayList.add(holidayObj2);
+		return holidayList;
+	}
+	
+	@RequestMapping("/holidayandemployeelist")
+	@ResponseBody
+	public List<Object> bothEmpListAndHolidayList(ServletRequest request) {	
+		List<Object> holidayList=new ArrayList<Object>();
+		Map<String,String> holidayObj1=new HashMap<String,String>();
+		holidayObj1.put("Date", "1-1-2019");
+		holidayObj1.put("reason", "New year");
+		holidayList.add(holidayObj1);
+		Map<String,String> holidayObj2=new HashMap<String,String>();
+		holidayObj2.put("Date", "25-12-2019");
+		holidayObj2.put("reason", "Christmas Day");
+		holidayList.add(holidayObj2);
+		return holidayList;
+	}
+	
+	// DenyAll -> Deactivates the rest service.
+	
+	@RequestMapping("/leavetype")
+	@ResponseBody
+	public List<String> leaveType() {
+		List<String> leaveTypeList=new ArrayList<String>();
+		leaveTypeList.add("Vacation Leave");
+		leaveTypeList.add("Sick Leave");
+		return leaveTypeList;
+	}
+}
