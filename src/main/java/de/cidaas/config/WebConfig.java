7cf339bfa860db0dccfb946acf6220fcb9f3c972
Daniel Storch <daniel.storch@widas.de>
2020-07-13 12:24:00 +0200

    remove unused imports

--- src/main/java/de/cidaas/config/WebConfig.java
+++ src/main/java/de/cidaas/config/WebConfig.java
@@ -3,8 +3,6 @@ package de.cidaas.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
