b6641c1e7eff031ebd33dcdd498c2a7671dc99fd
Daniel Storch <daniel.storch@widas.de>
2020-08-10 12:17:57 +0200

    Remove client_secret from method

--- src/main/java/de/cidaas/config/WebSecurityConfig.java
+++ src/main/java/de/cidaas/config/WebSecurityConfig.java
@@ -43,7 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors();
 		JwtSpringInterceptor
 		.offlineValidation(env.getProperty("client_id"), env.getProperty("base_url"))
-//		.introspectionValidation(env.getProperty("client_id"), env.getProperty("base_url"), env.getProperty("client_secret"))
+//		.introspectionValidation(env.getProperty("client_id"), env.getProperty("base_url"))
 		.configure(http).antMatcher("/**")  
         .authorizeRequests()  
         .antMatchers("/").permitAll()  
